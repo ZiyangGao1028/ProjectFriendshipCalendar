120b1dc25e45bb0240980344290c29f3cd0ffe0f
ziyanggao1028 <ziyanggao1028@gmail.com>
2018-05-26 21:59:57 -0500

    initial menu bar

--- app/src/main/java/com/example/gaoziyang/projectfriendshipcalendar/MainActivity.java
+++ app/src/main/java/com/example/gaoziyang/projectfriendshipcalendar/MainActivity.java
@@ -1,13 +1,115 @@
 package com.example.gaoziyang.projectfriendshipcalendar;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.database.DataSetObserver;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle toggle;
+    private String mActivityTitle;
+
+    @SuppressLint("ResourceType")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        drawerLayout = findViewById(R.id.drawer_layout);
+        mActivityTitle = getTitle().toString();
+
+        toggle = new ActionBarDrawerToggle(this, drawerLayout,
+                R.string.openDrawer, R.string.closeDrawer) {
+
+            /** Called when a drawer has settled in a completely open state. */
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                getSupportActionBar().setTitle("Back");
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+
+            /** Called when a drawer has settled in a completely closed state. */
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+                getSupportActionBar().setTitle(mActivityTitle);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+        };        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        drawerLayout.setDrawerListener(toggle);
+        toggle.syncState();
+        createNavigation();
+
+    }
+
+    private void createNavigation() {
+        NavigationView navigationView = findViewById(R.id.nav_view2);
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        menuItem.setChecked(true);
+                        switch (menuItem.getItemId()) {
+                            case R.id.nav_hour:
+                                Intent books = new Intent(MainActivity.this, MainActivity.class);
+                                startActivity(books);
+                                drawerLayout.closeDrawer(GravityCompat.START);
+                                break;
+                            case R.id.resources:
+                                drawerLayout.closeDrawer(GravityCompat.START);
+                                break;
+                            case R.id.settings:
+                                drawerLayout.closeDrawer(GravityCompat.START);
+                                break;
+                            case R.id.help:
+                                drawerLayout.closeDrawer(GravityCompat.START);
+                                break;
+                            case R.id.signOut:
+                                //Intent myitems1 = new Intent(MainActivity.this, MyitemsActivity1.class);
+                                //startActivity(myitems1);
+                                drawerLayout.closeDrawer(GravityCompat.START);
+                                break;
+                        }
+                        return false;                    }
+                }
+        );
+    }
+
+    private void setupDrawer() {
+        toggle.setDrawerIndicatorEnabled(true);
+        drawerLayout.setDrawerListener(toggle);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(toggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
